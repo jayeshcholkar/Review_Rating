1578c1fb611ee54303d0bc9cbf44c8b239a0dfe5
jayeshcholkar <115981022+jayeshcholkar@users.noreply.github.com>
2023-12-16 16:12:52 +0530

    update routes

--- routes/mainRoutes/mainRoutes.js
+++ routes/mainRoutes/mainRoutes.js
@@ -5,8 +5,8 @@ const companyRouter = require("../companyRouter");
 const validateToken = require("../../middleware/validateToken");
 
 router.use("/user", userRouter);
-router.use(validateToken); // do this for using validation for all routes
-router.use("/review", reviewRouter);
-router.use("/company", companyRouter);
+// router.use(validateToken); // do this for using validation for all routes
+router.use("/review", validateToken, reviewRouter);
+router.use("/company", validateToken, companyRouter);
 
 module.exports = router;
