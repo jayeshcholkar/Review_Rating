5da752a10cdf1c04ba4dc4052797eb40bd662356
jayeshcholkar <115981022+jayeshcholkar@users.noreply.github.com>
2023-12-16 15:30:28 +0530

    Update .gitignor

--- models/config.js
+++ models/config.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 
 mongoose.connect(process.env.URL, { useNewUrlParser: true });
 mongoose.connection.on("connected", (err, res) => {
-  console.log(`mongoDb is connected to Review_Rating`);
+  console.log(`mongoDb is connected to Review_Rating`); 
 });
 mongoose.connection.on("error", (err, res) => {
   console.log(`mongoDb connection error ${err}`);
